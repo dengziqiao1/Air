bfe06928c99be2e3967c82ace47b3bc408bf1332
dzq <18629477078@163.com>
2018-11-29 22:20:13 +0800

    12545

--- app/src/main/java/com/fce/air/AirActivity.java
+++ app/src/main/java/com/fce/air/AirActivity.java
@@ -3,11 +3,12 @@ package com.fce.air;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class AirActivity extends Activity {
     protected TextView open;
-    protected TextView close;
+      private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,25 @@ public class AirActivity extends Activity {
      * 初始化View
      */
     private void initView() {
+        open = findViewById(R.id.air_seekBar_text);
+        seekBar = findViewById(R.id.air_seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
+                open.setY(-progress*5.5f);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
 
